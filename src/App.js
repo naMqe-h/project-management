7d03ae87fc4476af5b808e6064c887302ea98296
naMqe <naMqe07@gmail.com>
2021-12-26 15:50:26 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Project from './pages/project/Project'
+import Signup from './pages/signup/Signup'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/projects/:id' element={<Project />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
