35de5a1c4ed82593b4097b55902f41f9dfda2a7a
naMqe <naMqe07@gmail.com>
2021-12-27 19:36:30 +0100

    added fetching projects

---
+++ src/components/ProjectList.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+import Avatar from './Avatar'
+import './ProjectList.css'
+
+export default function ProjectList({ projects }) {
+    return (
+        <div className='project-list'>
+            {projects.length === 0 && <p>No projects yet</p>}
+            {projects.map(project => (
+                <Link to={`/projects/${project.id}`} key={project.id}>
+                    <h4>{project.name}</h4>
+                    <p>Due by {project.dueDate.toDate().toDateString()}</p>
+                    <div className="assigned-to">
+                        <ul>
+                            {project.assignedUsersList.map(user => (
+                                <li key={user.photoURL}>
+                                    <Avatar src={user.photoURL} />
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    )
+}
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,15 @@
+import ProjectList from '../../components/ProjectList'
+import { useCollection } from '../../hooks/useCollection'
 import './Dashboard.css'
 
 export default function Dashboard() {
+    const { documents, error } = useCollection('projects')
+
     return (
         <div>
-            Dashboard
+            <h2 className="page-title">Dashboard</h2>
+            {error && <p className='error'>{error}</p>}
+            {documents && <ProjectList projects={documents} />}
         </div>
     )
 }
