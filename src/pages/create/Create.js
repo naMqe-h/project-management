623d88a0815fda297ea33a819128e9f3d1dfc6d1
naMqe <naMqe07@gmail.com>
2021-12-27 03:03:49 +0100

    added create project form template

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,87 @@
+import { useEffect, useState } from 'react'
+import Select from 'react-select'
+import { useCollection } from '../../hooks/useCollection'
 import './Create.css'
 
+const categories = [
+    { value: 'development', label: 'Development' },
+    { value: 'design', label: 'Design' },
+    { value: 'sales', label: 'Sales' },
+    { value: 'marketing', label: 'Marketing' },
+]
+
 export default function Create() {
+    const { documents } = useCollection('users')
+    const [users, setUsers ] = useState([])
+
+    const [name, setName] = useState('')
+    const [details, setDetails] = useState('')
+    const [dueDate, setDueDate] = useState('')
+    const [category, setCategory] = useState('')
+    const [assignedUsers, setAssignedUsers] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(name, details, dueDate, category.value, assignedUsers)
+    }
+
+    useEffect(() => {
+        setUsers([])
+        const options = documents?.map(user => {
+            return { value: user, label: user.displayName }
+        })
+        setUsers(options)
+    }, [documents])
+
     return (
-        <div>
-            Create
+        <div className='create-form'>
+            <h2 className="page-title">Create a new project</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Project name:</span>
+                    <input 
+                        required
+                        type="text" 
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
+                </label>
+                <label>
+                    <span>Project details:</span>
+                    <textarea 
+                        required
+                        type="text" 
+                        onChange={(e) => setDetails(e.target.value)}
+                        value={details}
+                    ></textarea>
+                </label>
+                <label>
+                    <span>Set due date:</span>
+                    <input 
+                        required
+                        type="date" 
+                        onChange={(e) => setDueDate(e.target.value)}
+                        value={dueDate}
+                    />
+                </label>
+                <label>
+                    <span>Project category:</span>
+                    <Select 
+                        options={categories}
+                        onChange={(option) => setCategory(option)}
+                    />
+                </label>
+                <label>
+                    <span>Assigned to:</span>
+                    <Select 
+                        options={users}
+                        onChange={(option) => setAssignedUsers(option)}
+                        isMulti
+                    />
+                </label>
+
+                <button className="btn">Add Project</button>
+            </form>
         </div>
     )
 }
