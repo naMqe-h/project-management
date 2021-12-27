0c0c920d27674f41cedb42d729862e56146fc4c1
naMqe <naMqe07@gmail.com>
2021-12-28 00:52:15 +0100

    added updateDocument function to useFirestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -14,11 +14,13 @@ const firestoreReducer = (state, action) => {
             return { isPending: true, document: null, success: null, error: null  }
         case 'ADDED_DOCUMENT':
             return { isPending: false, document: action.payload, success: true, error: null }
-        case 'ERROR':
-            return { isPending: false, document: null, success: false, error: action.payload }
         case 'DELETED_DOCUMENT':
             return { isPending: false, document: null, success: true, error: null }
-
+        case 'UPDATED_DOCUMENT':
+            return { isPending: false, document: action.payload, success: true, error: null }
+        case 'ERROR':
+            return { isPending: false, document: null, success: false, error: action.payload }
+        
         default:
             return state 
     }
@@ -60,10 +62,23 @@ export const useFirestore = (collection) => {
         }     
     }
 
+    const updateDocument = async (id, updates) => {
+        dispatch({ type: 'IS_PENDING' })
+
+        try {
+            const updatedDocument = await ref.doc(id).update(updates)
+            dispatchIfNotCancelled({ type: 'UPDATED_DOCUMENT', payload: updatedDocument })
+            return updatedDocument
+        } catch (err) {
+            dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
+            return null
+        }
+    }
+
     useEffect(() => {
         return () => setIsCancelled(true)
     }, [])
 
-    return { addDocument, deleteDocument, response }
+    return { addDocument, deleteDocument, updateDocument, response }
 
 }
