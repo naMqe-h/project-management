3719c570903448ac5a6a467a463f38bdd2a96af9
naMqe <naMqe07@gmail.com>
2021-12-29 15:45:22 +0100

    added deleting projects

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,10 +1,23 @@
 import Avatar from '../../components/Avatar'
+import { Navigate, useNavigate } from 'react-router-dom'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 export default function ProjectSummary({ project }) {
+    const navigate = useNavigate()
+    const { deleteDocument } = useFirestore('projects')
+    const { user } = useAuthContext()
+
+    const handleClick = () => {
+        deleteDocument(project.id)
+        navigate('/')
+    }
+
     return (
         <div>
             <div className="project-summary">
                 <h2 className="page-title">{project.name}</h2>
+                <p>By: {project.createdBy.displayName}</p>
                 <p className="due-date">Project due by {project.dueDate.toDate().toDateString()}</p>
                 <p className="details">{project.details}</p>
                 <h4>Project is assigned to:</h4>
@@ -16,6 +29,7 @@ export default function ProjectSummary({ project }) {
                     ))}
                 </div>
             </div>
+            {user.uid === project.createdBy.id && <button className="btn" onClick={handleClick}>Mark as Complete</button>}  
         </div>
     )
 }
